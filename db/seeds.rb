def8af9405043a9e5f2c9c7adb502cccd2b71269
seif <seif1994@yahoo.fr>
2019-07-19 15:37:06 +0200

    create requests sccafold and db seeds for some users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(email: 'seif@yahoo.com', nickname: 'eddin', name: 'seif', password: "seifseif")
+User.create(email: 'sahnoun@yahoo.com', nickname: 'sahnoun', name: 'sahnoun', password: "seifseif")
